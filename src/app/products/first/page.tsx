382bdeb9ac599ffbf4d28451ce7203c3e806ec6d
Naga Raju K <nagaraju.k@nichebit.com>
2025-01-03 11:14:02 +0530

    nested routing

---
+++ src/app/products/first/page.tsx
@@ -0,0 +1,9 @@
+
+export default function FirstProduct() {
+    return (
+        <div>
+            <h1>FirstProduct</h1>
+        </div>
+    );
+  }
+  
---
+++ src/app/products/second/page.tsx
@@ -0,0 +1,9 @@
+
+export default function SecondProduct() {
+    return (
+        <div>
+            <h1>SecondProduct</h1>
+        </div>
+    );
+  }
+  
